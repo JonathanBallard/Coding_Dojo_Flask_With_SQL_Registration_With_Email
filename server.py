bc79b845f85c9a63e4ef371b94bc77c80b22dd56
Jonathan Ballard <asgardschosen@gmail.com>
2020-01-28 21:07:06 -0800

    finished

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconnection import connectToMySQL    # import the function that will return an instance of a connection
+import re
 
 app = Flask(__name__)
 app.secret_key = "secretstuff"
@@ -18,44 +19,60 @@ def register():
     mysql = connectToMySQL("basic_registration")
     users = mysql.query_db("SELECT * FROM users;")
     print(users)
-
+    EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+    PW_REGEX = re.compile(r'^.*(?=.{8,10})(?=.*[a-zA-Z])(?=.*?[A-Z])(?=.*\d)[a-zA-Z0-9!@£$%^&*()_+={}?:~\[\]]+$')
     firstName = request.form['first_name']
     lastName = request.form['last_name']
+    email = request.form['email']
     password = request.form['password']
     conPassword = request.form['passwordConfirm']
     isValid = True
 
     if len(firstName) <= 0:
         isValid = False
-        flash('Please enter a first name')
+        flash('Please enter a first name', 'name')
 
     if not firstName.isalpha():
         isValid = False
-        flash('Please enter a first name using only alphabetic characters')
+        flash('Please enter a first name using only alphabetic characters', 'name')
 
     if len(lastName) <= 0:
         isValid = False
-        flash('Please enter a last name using only alphabetic characters')
+        flash('Please enter a last name', 'name')
 
     if not lastName.isalpha():
         isValid = False
+        flash('Please enter a last name using only alphabetic characters', 'name')
+
+    if len(email) <= 3:
+        isValid = False
+        flash('Please enter an email address', 'email')
+
+    if not EMAIL_REGEX.match(request.form['email']):
+        isValid = False
+        flash("Invalid email address!", 'email')
+
+    if not PW_REGEX.match(request.form['password']):
+        isValid = False
+        flash("Invalid password! Minimum 8 characters, 1 number, and 1 special character", 'password')
 
     if len(password) <= 4:
         isValid = False
-        flash('Please enter a valid password (minimum 5 characters)')
+        flash('Please enter a valid password (minimum 5 characters)', 'password')
 
     if not password == conPassword:
         isValid = False
-        flash('Password doesnt match confirm password')
+        flash('Password doesnt match confirm password', 'password')
 
     
     
     if isValid == True:
         mysql = connectToMySQL("basic_registration")
-        query = "INSERT INTO users (first_name, last_name, password) VALUES (%(fname)s, %(lname)s, %(pw)s);"
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(fname)s, %(lname)s, %(email)s, %(pw)s);"
         data = {
             "fname": firstName,
             "lname": lastName,
+            "email": email,
             "pw": password
         }
         new_user_id = mysql.query_db(query, data)
